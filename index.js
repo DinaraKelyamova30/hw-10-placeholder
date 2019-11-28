70f9a3ba656e17e5bbfb48a605258d8a55b70877
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-11-28 21:42:31 +0200

    js

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ function moveText() {
     if (input1.value == "") {
         input2.value;
     } else {
-        input2.value = input1.value;
+        input2.placeholder = input1.value;
         input1.value = "";
     }
 }
-
 move.onmousemove = function () {
     move.style.backgroundColor = 'red';
 }
-
 move.onmouseleave = function () {
     move.style.backgroundColor = 'rgb(0, 255, 98)';
 }
 
 // переміщення значення в placeholder
-function newAtt() {
-    place.placeholder = "newAttr";
-};
-place.placeholder = place.value;
+place.addEventListener("focusout", focus);
+
+function focus(){
+    place.placeholder = place.value;
+    place.value = "";
+}
