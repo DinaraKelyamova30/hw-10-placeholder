0f70bab60d1a856761ab4c4489dd17255891ee03
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-11-28 13:39:13 +0200

    js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const input1 = document.querySelector('.input1');
 const input2 = document.querySelector('.input2');
 const move = document.querySelector('.btn');
+const place = document.querySelector('#placeholder');
 
+// переміщення значення в інший input
 function moveText() {
-    if (input1 == "") {
-        input2.value = "";
-    } else if (input1 != "") {
-        input1.value = input2.value;
+    if (input1.value == "") {
+        input2.value;
+    } else {
+        input2.value = input1.value;
+        input1.value = "";
     }
 }
 
+move.onmousemove = function () {
+    move.style.backgroundColor = 'red';
+}
+
+move.onmouseleave = function () {
+    move.style.backgroundColor = 'rgb(0, 255, 98)';
+}
+
+// переміщення значення в placeholder
+function newAtt() {
+    place.placeholder = "newAttr";
+};
+place.placeholder = place.value;
